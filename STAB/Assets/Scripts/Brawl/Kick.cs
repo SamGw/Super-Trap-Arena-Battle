98ebb59278d54df8fef10be6a4e5785fa7b39261
Gwynne Sam <sam.gwynne@epita.fr>
2019-04-23 21:01:16 +0200

    Stable

---
+++ STAB/Assets/Scripts/Brawl/Kick.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Kick : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Colliders"))
+        {
+            if (other.transform.parent.parent.CompareTag("Player"))
+            {
+                PlayerMovements other_script = other.GetComponentInParent<PlayerMovements>();
+                other_script.hitStun = true;
+            }
+            else
+            {
+                IA other_script = other.GetComponentInParent<IA>();
+                other_script.hitStun = true;
+            }
+
+            //Ajouter pourcentage
+
+                //
+
+                //Knockback
+                Rigidbody2D other_rb2d = other.GetComponentInParent<Rigidbody2D>();
+                Transform tr = transform.parent.parent;
+                float knockback;
+
+                if (CompareTag("Attack Side"))
+                {
+                    knockback = tr.localScale.x * 40;
+                    other_rb2d.velocity = new Vector2(knockback, other_rb2d.velocity.y);
+                }
+                else if (CompareTag("Attack Up"))
+                {
+                    knockback = 40;
+                    other_rb2d.velocity = new Vector2(other_rb2d.velocity.x, knockback);
+                }
+            //Sounds effects
+            
+            SoundEffects.Instance.Make_FaceSound();
+
+        }
+    }
+}
