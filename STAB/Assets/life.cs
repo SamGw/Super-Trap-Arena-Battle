9949a989a6acbf4f4a76bd0b90d0ce9911ab67ac
hanniv <anvinhlala@hotmail.fr>
2019-06-04 14:01:29 +0200

    commit

--- STAB/Assets/life.cs
+++ STAB/Assets/life.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class life : MonoBehaviour
 {
     public TextMeshProUGUI life_Text1; 
+    public TextMeshProUGUI life_Text11; 
     public TextMeshProUGUI life_Text2;  
+    public TextMeshProUGUI life_Text21;  
     public TextMeshProUGUI life_Text3;
+    public TextMeshProUGUI life_Text31;
     public TextMeshProUGUI life_Text4;
+    public TextMeshProUGUI life_Text41;
+
+    public Image coeur1;
+    public Image coeur2;
+    public Image coeur3;
+    public Image coeur4;
     
     private int nb_player ;
     // Start is called before the first frame update
     void Start()
     {
         nb_player = main.numberOfPlayers;
+        coeur1.enabled = false;
+        coeur2.enabled = false;
+        coeur3.enabled = false;
+        coeur4.enabled = false;
     }
 
     // Update is called once per frame
@@ -22,42 +36,55 @@ public class life : MonoBehaviour
     {
         if (nb_player >=1)
         {
+            coeur1.enabled = true;
+            life_Text1.text = "P1" ;   
             int l1 = DestroyByBoundary.life1;
             string str = l1.ToString();
-            life_Text1.text = "player1 \n" +"life:"+ str ;                    
+            life_Text11.text = str;
+
         }
 
         if (nb_player >=2)
         {
+            coeur2.enabled = true;
+            life_Text2.text = "P2" ;   
             int l2 = DestroyByBoundary.life2;
-            string str2 = l2.ToString();
-            life_Text2.text = "player2 \n" +"life:"+ str2 ;                     
+            string str = l2.ToString();
+            life_Text21.text = str;                     
         }
         else
         {
             life_Text2.text = " ";
+            life_Text21.text = " ";
         }
 
         if (nb_player >= 3)
-        {           
+        {        
+            
+            coeur3.enabled = true;
+            life_Text3.text = "P3" ;   
             int l3 = DestroyByBoundary.life3;
-            string str3 = l3.ToString();
-            life_Text3.text = "player3 \n" +"life:"+ str3 ; 
+            string str = l3.ToString();
+            life_Text31.text = str;                     
         }
         else
         {
             life_Text3.text = " ";
+            life_Text31.text = " ";
         }
 
         if (nb_player >= 4)
         {
+            coeur4.enabled = true;
+            life_Text4.text = "P4" ;   
             int l4 = DestroyByBoundary.life4;
-            string str4 = l4.ToString();
-            life_Text4.text = "player4 \n" +"life:"+ str4 ;             
+            string str = l4.ToString();
+            life_Text41.text = str;                     
         }
         else
         {
             life_Text4.text = " ";
+            life_Text41.text = " ";
         }
     }
 }
